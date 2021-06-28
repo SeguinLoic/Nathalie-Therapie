cf56cbc8a21355d44fdb9e5026cce0022378b802
Loïc Seguin <loicseguin74@gmail.com>
2021-06-28 13:40:35 +0200

    Section site en construction

--- pages/fun.vue
+++
@@ -1,6 +0,0 @@
-<template>
-    <main>
-        <h1>Fun page</h1>
-        <NuxtLink to="/">Lien vers la home</NuxtLink>
-    </main>
-</template>
--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,16 @@
 <template>
-    <main>
-        <h1>Home Page</h1>
-        <NuxtLink to="/fun">Lien vers la fun</NuxtLink>
-    </main>
+    <div class="home">
+        <img id="logo" src="~/assets/images/logo-vertical.svg" />
+        <p class="slogan">Site en cours de construction...</p>
+        <div class="contact">
+            <a href="tel:0610646323" class="tile-contact">
+                <img src='~/assets/images/phone.svg'>
+                <span>06 10 64 63 23</span>
+            </a>
+            <a href="mailto:contact@nathalietherapie.fr" class="tile-contact">
+                <img src='~/assets/images/mail.svg'>
+                <span>contact@nathalietherapie.fr</span>
+            </a>
+        </div>
+    </div>
 </template>
