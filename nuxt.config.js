b37a7196e66a76f912457f22af4713a83b486b71
Loïc Seguin <loicseguin74@gmail.com>
2021-07-03 18:45:58 +0200

    Modification adresse mail

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,5 +1,20 @@
 export default {
     target: 'static',
+    head: {
+        title: 'Title de mon site',
+        meta: [
+            { charset: 'utf-8' },
+            { name: 'viewport', content: 'width=device-width, initial-scale=1'},
+            {
+                hid: 'description',
+                name: 'description',
+                content: 'La description de mon site web'
+            }
+        ],
+        link: [
+            { rel: 'icon', type: 'image/x-icon', href: '/FAVICON.png' }
+        ]
+    },
     css: [
         '~/assets/css/main.css'
     ]
